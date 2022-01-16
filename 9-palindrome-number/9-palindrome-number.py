b7d9e00d7d1ccd81308fac6d988afd1e218897ec
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-01-17 01:32:19 +0900

    Time: 148 ms (5.08%), Space: 14.1 MB (77.39%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        input = []
+        while x >= 10:
+            input.append(x % 10)
+            x //= 10
+    
+        input.append(x)
+        print(input)
+
+
+        listlen = len(input)
+        half = listlen/2
+        i = 0
+        while(i < half):
+            if input[i] != input[listlen - i - 1]:
+                return False
+            i += 1
+        
+        return True

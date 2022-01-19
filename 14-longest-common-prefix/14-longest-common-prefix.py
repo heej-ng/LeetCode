c5b785fdecf07125825ec4e518156399b5310cf6
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-01-19 22:30:42 +0900

    Time: 73 ms (5.13%), Space: 14.1 MB (94.17%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        #strs = ["flower","flow","flight"]
+        pre = strs[0]
+        for i in range(1, len(strs)):
+            common = ""
+            minlen = len(pre)
+            if minlen > len(strs[i]):
+                minlen = len(strs[i])
+                
+            for j in range(minlen):
+                if pre[j] != strs[i][j]:  # 다른 부분 시작
+                    common = pre[0:j]
+                    break
+                    
+                elif j == minlen-1:        # 끝까지 같은 경우
+                    common = pre[0:minlen]
+                    
+            pre = common
+
+        return pre

d47949b50fcd26b38f7536c69ad008517ddc5cb1
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-01-31 05:55:35 +0900

    Time: 32 ms (81.82%), Space: 14 MB (95.23%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,19 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        nega_flag = 0
+        sign_flag = 1
         if x < 0:
-            nega_flag = 1
+            sign_flag = -1
             x *= -1
-        list = []
+        result = 0
         while x > 0:
-            list.append(x%10)
+            result = result * 10 + (x%10)
             x //= 10
-        leng = len(list)
-        k = 0
-        for i in range(leng):
-            k += list[i] * (10**(leng-1-i))
-        if nega_flag == 1:
-            k *= -1
-        if k >= 2**31 or k < -1*(2**31):
-            k = 0
-        return k
+        result *= sign_flag
+        if result >= 2**31 or result < -1*(2**31):
+            return 0
+        return result

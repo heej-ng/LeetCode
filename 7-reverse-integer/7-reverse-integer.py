0685fa68f02a18a3ad24cfd378695bebf1bc8e26
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-01-31 05:56:09 +0900

    Time: 28 ms (93.54%), Space: 13.8 MB (98.69%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -5,10 +5,11 @@ class Solution:
             sign_flag = -1
             x *= -1
         result = 0
+        out_bound = 2**31
         while x > 0:
             result = result * 10 + (x%10)
             x //= 10
         result *= sign_flag
-        if result >= 2**31 or result < -1*(2**31):
+        if result >= out_bound or result < -1*out_bound:
             return 0
         return result

e142d08a78ccf7c13b1604dd21cf992e6627a1ec
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-02-05 13:24:21 +0900

    Time: 87 ms (18.08%), Space: 13.9 MB (90.73%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,7 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dic = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
-        
         if len(s) == 1:
             return dic[s]
         value = 0
@@ -14,5 +13,4 @@ class Solution:
                 value -= ptr
             if i <= len(s)-3:
                 post = dic[s[i+2]]
-        
         return value

2eb001bc72e5fd219f8c0c89b453a4a369e01e0f
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-02-06 13:38:55 +0900

    Time: 73 ms (47.55%), Space: 13.8 MB (96.82%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         length = len(nums)
         dic = {}
-        k = 0
         i = 0
         while i < length:
             if nums[i] in dic:          # dic 안에 nums[i] key가 이미 존재
@@ -12,9 +11,7 @@ class Solution:
                 else:                   # value가 1번만 존재했던 경우
                     dic[nums[i]] = 2
                     i += 1
-                    k += 1
             else:
                 dic[nums[i]] = 1
                 i += 1
-                k += 1
         return i

757fa5196c2eab50b9fb0966a26f909d265f4216
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-02-10 18:19:12 +0900

    Time: 16 ms (99.95%), Space: 13.8 MB (96.14%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -3,21 +3,18 @@ class Solution:
         if digits == "":
             return []
         dic = { "2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz" }
-        # result = ["a","b","c"]
         result = [""]
-        # list = []
-        # digits = "2"
         for i in digits:
             digit = dic[i]
             print(digit)
             list = []
             for j in digit:     # j : (d or e or f)
                 for k in range(len(result)):
-                    list2 = []
+                    # list2 = []
                     temp = result[k]
                     temp += j
-                    #result[k] = temp
-                    list2.append(temp)
-                    list += list2
+                    # list2.append(temp)
+                    list.append(temp)
+                    # list += list2
             result = list
         return result

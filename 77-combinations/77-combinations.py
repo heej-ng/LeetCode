7c21b1e57c118aeb3a3d530dac64b2bdc8ab832c
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-03-23 18:59:53 +0900

    Time: 477 ms (62.74%), Space: 16 MB (19.12%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        
+        def dfs(element, start: int, k: int):
+            # leaf node인 경우
+            if k == 0:
+                result.append(element[:])
+                return
+            
+            
+            for i in range(start, n+1): # start ~ n
+                element.append(i)
+                dfs(element, i+1, k-1)
+                element.pop()
+                
+        dfs([],1,k)
+        return result

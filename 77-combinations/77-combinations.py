d11104b3a189ab459b9cf505914c3b86b1e6e22c
Park heejung <80497254+heej-ng@users.noreply.github.com>
2022-03-29 15:47:24 +0900

    Time: 412 ms (77.99%), Space: 16 MB (20.47%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -3,15 +3,17 @@ class Solution:
         result = []
         
         def dfs(element, start: int, k: int):
-            # leaf node인 경우
+            
             if k == 0:
                 result.append(element[:])
                 return
-            
-            for i in range(start, n+1): # start ~ n
+                
+            for i in range(start, n+1):
                 element.append(i)
                 dfs(element, i+1, k-1)
                 element.pop()
                 
         dfs([],1,k)
         return result
+            
+            
